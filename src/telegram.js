dc7ae2adb983867879b269f504de2ebcf2838992
nitin.sharma <nitin10.sharma@ril.com>
2021-06-28 01:06:23 +0530

    logs

--- src/telegram.js
+++ src/telegram.js
@@ -8,18 +8,22 @@ const bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
+    console.log('Request came from ',chatId);
     const msgText = msg.text.substr(1);
+    console.log('Msg ', msgText);
     if (!msgText) {
         bot.sendMessage(chatId, 'Please select any command by typing /');
         return;
     }
     const row = indices.filter(item => item.command === msgText);
+    console.log(row);
     if (!row.length) {
         bot.sendMessage(chatId, 'Currently we do not support this indices');
         return;
     }
     getAllRSI(row).then(res => {
         const data = `RSI of ${res}`;
+        console.log(data);
         bot.sendMessage(chatId, data);
     }).catch(err => {
         console.log(err);
@@ -29,7 +33,7 @@ bot.on('message', (msg) => {
 
 const sendMsg = (text) => {
     try {
-        bot.sendMessage('@rsi_indices_channel', text);
+        bot.sendMessage('-555705163', text);
     } catch (error) {
         console.log(error);
     }
